fa63695c9fd9f4b06402799e4c65b715c803a336
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-11-10 19:13:47 +0530

    Calculate Area

---
+++ Conditional_And_Iterative_Statement/CalculateArea.java
@@ -0,0 +1,8 @@
+class CalculateArea{
+    public static void main(String args[]){
+        int a = 5;
+        int area = (int )(1.76 * a * a )/4;
+        //System.out.println("Area of triangle is : " + ((1.76 *a*a)/4 ));
+        System.out.println(area);
+    }
+}
