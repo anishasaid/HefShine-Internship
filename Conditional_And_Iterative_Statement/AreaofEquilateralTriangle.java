784b0cd986ac3902ff3097591dde649ae71104a2
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-11-10 19:12:38 +0530

    Area of Equilateral Triangle

---
+++ Conditional_And_Iterative_Statement/AreaofEquilateralTriangle.java
@@ -0,0 +1,8 @@
+public class AreaofEquilateralTriangle {
+    public static void main(String[] args) {
+        int a = 5 ;
+        double sqroot = Math.sqrt(3);
+        double area = sqroot/4 * (a *a);
+        System.out.println("Area is : "+ area);
+    }
+}
