d3a8e14ff4e732f5578a1b0e0da61cd8c6eea4dd
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-11-11 11:04:38 +0530

    Calculate Profit

---
+++ Conditional_And_Iterative_Statement/CalculateProfit.java
@@ -0,0 +1,11 @@
+public class CalculateProfit {
+    public static void main(String[] args) {
+        int buyPrice = 20;
+        int sellPrice = 30;
+        if(sellPrice > buyPrice){
+            System.out.println("Profit");
+        }else{
+            System.out.println("loss");
+        }
+    }
+}
