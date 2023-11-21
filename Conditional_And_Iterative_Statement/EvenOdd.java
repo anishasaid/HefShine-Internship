f1131c31754bbe2d8167d210ad16295d386fd654
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-11-21 19:20:14 +0530

    Find Number is Even/Odd

---
+++ Conditional_And_Iterative_Statement/EvenOdd.java
@@ -0,0 +1,10 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num = 9;
+        if(num % 2 == 0){
+            System.out.println(num +" number is even");
+        }else{
+            System.out.println(num+ " number is odd");
+        }
+    }
+}
