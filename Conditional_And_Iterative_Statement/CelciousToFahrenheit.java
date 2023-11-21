42a6785ffdb6bf7dcd096577aadfce911463335f
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-11-21 19:18:31 +0530

    Conversion of Celcious to Fahrenheit

---
+++ Conditional_And_Iterative_Statement/CelciousToFahrenheit.java
@@ -0,0 +1,8 @@
+// convert celcious to fahrenheit and viceversa.
+public class CelciousToFahrenheit {
+    public static void main(String[] args) {
+        int c = 10;
+        double f = 32 + (9/5) * c;
+        System.out.println(" celcious to fahrenheit is : "+ f);
+    }
+}
