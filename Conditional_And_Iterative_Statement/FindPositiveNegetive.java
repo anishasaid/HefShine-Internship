dc19dba979f325da8567e4510ae3caf9a94b0144
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-11-26 14:03:55 +0530

    Find Whether the number is Positive or Negative Number

---
+++ Conditional_And_Iterative_Statement/FindPositiveNegetive.java
@@ -0,0 +1,12 @@
+public class FindPositiveNegetive {
+    public static void main(String[] args) {
+        int num = 3;
+        if(num > 0)
+            System.out.println("positive number");
+        else if(num < 0 )
+            System.out.println("negative number");
+        else
+            System.out.println("number is 0");
+        
+    }
+}
