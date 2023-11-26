b89400d1ed72f651824b16722914c6cc866b3605
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-11-26 14:00:59 +0530

    Find Minimum and Maximum value

---
+++ Conditional_And_Iterative_Statement/FindMaxMin.java
@@ -0,0 +1,15 @@
+// Find which number is maximum and minimum
+public class FindMaxMin {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        if(num1 > num2){
+            System.out.println("num1 is max");
+            System.out.println("num2 is min");
+        }else{
+            System.out.println("num2 is max");
+            System.out.println("num1 is min");
+        }
+    }
+}
