d8ade66bb6015951e9fc1b61b85da348da9f0d1e
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-11-30 21:08:07 +0530

    Add files via upload

---
+++ Conditional_And_Iterative_Statement/MyClass.java
@@ -0,0 +1,11 @@
+class MyClass{
+
+    public static void main(String [] args){
+
+        int side = 5;
+        int area = side * side;
+        int perimeter = (4*side);
+        System.out.println("Area of square is : "+ area);
+        System.out.println("Premeter of the square is : "+ perimeter);
+    }
+}
