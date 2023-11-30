3680dc846e4a165f2c1e56dd7add03f5b2d9d2ad
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-11-30 21:07:21 +0530

    Find whether the character is Upper Case Or Lower Case

---
+++ Conditional_And_Iterative_Statement/IsUpperCase.java
@@ -0,0 +1,10 @@
+public class IsUpperCase {
+    public static void main(String[] args) {
+        char ch = 'g';
+        if(ch >= 'A' && ch <= 'Z'){
+            System.out.println("Upper Case");
+        }else{
+            System.out.println("Lower Case ");
+        }
+    }
+}
