a5023af9a1a9e5450074286494ad9d9ac08327f0
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-12-01 10:17:28 +0530

    Add files via upload

---
+++ Conditional_And_Iterative_Statement/Program.java
@@ -0,0 +1,10 @@
+public class Program {
+    public static void main(String[] args) {
+        int num = 7;
+        if(num >= 1 && num <= 10){
+            System.out.println("yes");
+        }else{
+            System.out.println("no");
+        }
+    }
+}
