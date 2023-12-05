2f7e7c2aa868fbe9e30d1038624d3eb7bb68732f
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-12-05 22:02:47 +0530

    Iterative Statement - Find Factorial of  a Number

---
+++ Conditional_And_Iterative_Statement/Factorial.java
@@ -0,0 +1,12 @@
+
+// Find factorial 
+public class Factorial {
+    public static void main(String[] args) {
+        int ans = 1;
+        int fact = 4;
+        for(int i=1; i<=fact; i++){
+            ans = ans * i;
+        }
+        System.out.println(ans);
+    }
+}
