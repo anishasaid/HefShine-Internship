8c46e07ce9e8cc951e275f0a6b67f93108f3b078
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-12-06 15:44:51 +0530

    Find Prime Numbers in given range

---
+++ Conditional_And_Iterative_Statement/FindPrimeInRange.java
@@ -0,0 +1,20 @@
+
+// Find the prime numbers in a given range.
+public class FindPrimeInRange {
+    public static void main(String[] args) {
+        for(int j=1; j<=100; j++){
+            int num = j;
+            int count = 0;
+
+            for(int i=1; i<=num ; i++){
+                if(num%i == 0){
+                    count ++;
+                }
+            }
+            if(count == 2 ){
+                System.out.println(j);
+            }
+        }
+    }
+    
+}
