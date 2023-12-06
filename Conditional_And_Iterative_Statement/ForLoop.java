65c9d8e193847fa186683a8ad19c52e8e076b30d
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-12-06 15:45:38 +0530

    Introduction of for loop

---
+++ Conditional_And_Iterative_Statement/ForLoop.java
@@ -0,0 +1,13 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++){
+            System.out.println("hi");
+        }
+
+        int  n = 1;
+        int m = 10;
+        for(int i=n; i<= m ; i++){
+            System.out.println(i);
+        }
+    }
+}
