93d8db8fbf78f87be86853fa49b19a6b5370aae9
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-12-06 15:44:12 +0530

    Find the number which contain 7 at last position

---
+++ Conditional_And_Iterative_Statement/FindLastNo.java
@@ -0,0 +1,11 @@
+
+// Find the number which contain 7 at last position
+class FindLastNo{
+    public static void main(String[] args) {
+        for(int i= 100; i<=200; i++){
+            if(i%10 == 7){
+                System.out.println(i);
+            }
+        }
+    }
+}
