7c2aab80a4f96610f80b761f6d7dd85a9ff85267
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-12-08 18:25:36 +0530

    Iterative Statement - Nested For Loop Intro

---
+++ Conditional_And_Iterative_Statement/NestedForLoop.java
@@ -0,0 +1,28 @@
+class NestedForLoop {
+    
+    public static void main(String[] args) {
+        // for(int i=1; i<=3; i++){
+        //     for(int j=1 ; j<=3 ; j++){
+        //         System.out.println("hii");
+        //     }
+        // }
+
+        // int x , y ;
+        // x = 15 ; 
+        // y = 20;
+        // if(x > 15)
+        // if(y> 15){
+        //     System.out.println("y is " + y);
+        // }else
+        //      System.out.println("x is " + x);
+
+        // int a = 3;
+        // int b = 6;
+
+        // int c = a|b;
+        // int d = a&b;
+
+        // System.out.println(c + " " + d);
+
+    }
+}
