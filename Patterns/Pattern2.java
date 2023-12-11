c1bed7bf6d3816c44fcdf77ad271bfc69ffed712
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-12-11 21:17:10 +0530

    Star Pattern

---
+++ Patterns/Pattern2.java
@@ -0,0 +1,25 @@
+package Patterns;
+/*
+
+ *****
+ ****
+ ***
+ **
+ *
+ 
+ */
+public class Pattern2 {
+    public static void main(String[] args) {
+        int end = 5;
+        for(int i =1; i<=5; i++){
+            for(int j=1; j<=end; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+            end --;
+        }
+
+        System.out.println();
+        
+    }
+}
---
+++ Patterns/Pattern3.java
@@ -0,0 +1,33 @@
+package Patterns;
+/*
+
+ *****
+ ****
+ ***
+ **
+ *
+ **
+ ***
+ ****
+ *****
+
+ */
+public class Pattern3 {
+    public static void main(String[] args) {
+
+        int end = 5;
+        for(int i=1; i<=9; i++){
+            for(int j=1; j<=end; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+            if(i < 5){
+                end --;
+            }else{
+                end++;
+            }
+        }
+
+       
+    }
+}
