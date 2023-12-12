03d3daaf9ab76ad1f996f72eed16282d16f87c9f
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-12-12 21:29:57 +0530

    Star Pattern

---
+++ Patterns/Pttern8.java
@@ -0,0 +1,24 @@
+package Patterns;
+/*
+ *****
+  ****
+   ***
+    **
+     *
+ */
+public class Pttern8 {
+    public static void main(String[] args) {
+        int spaceCnt = 0;
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=5;j++){
+                if(j<=spaceCnt){
+                    System.out.print(" ");
+                }else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+            spaceCnt++;
+        }
+    }
+}
