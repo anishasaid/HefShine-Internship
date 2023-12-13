53e20487dc974da19774b67b54210979ff137851
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-12-13 21:31:39 +0530

    Star Pattern

---
+++ Patterns/Pattern9.java
@@ -0,0 +1,34 @@
+package Patterns;
+/*
+    *
+   **
+  ***
+ ****
+*****
+ ****
+  ***
+   **
+    *
+*/
+public class Pattern9 {
+    public static void main(String[] args) {
+        int spaceCnt = 4;
+        for(int i=1; i<=9; i++){
+            for(int j=1; j<=5; j++){
+               
+                if(j<=spaceCnt){
+                    System.out.print(" ");
+                }else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+            if(i<5){
+                spaceCnt--;
+            }else{
+                spaceCnt++;
+            }
+
+        }
+    }
+}
