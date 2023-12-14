faa0f5e55335e3e378ea7bdca004d183d8180d55
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-12-14 19:07:29 +0530

    Star Pattern

---
+++ Patterns/Pattern15.java
@@ -0,0 +1,27 @@
+package Patterns;
+
+/*
+*********
+ *******
+  *****
+   ***
+    *
+ */
+public class Pattern15 {
+    public static void main(String[] args) {
+        int spaceCnt = 0;
+        int end = 9;
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=end ; j++){
+                if(j<=spaceCnt){
+                    System.out.print(" ");
+                }else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+            spaceCnt++;
+            end--;
+        }
+    }
+}
