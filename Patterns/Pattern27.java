9ada3e40c4cc3b79acf4bbbca28907c1f4252ae0
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-12-21 13:27:23 +0530

    Add files via upload

---
+++ Patterns/Pattern27.java
@@ -0,0 +1,24 @@
+package Patterns;
+/*
+
+1
+22
+333
+4444
+55555
+
+ */
+public class Pattern27 {
+    public static void main(String[] args) {
+        int end = 1;
+        int num = 1;
+        for(int i=1;i<=5; i++){
+            for(int j=1; j<=end; j++){
+                System.out.print(num);
+            }
+            System.out.println();
+            end++;
+            num++;
+        }
+    }
+}
