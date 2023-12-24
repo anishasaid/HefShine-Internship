3b192cc87298082a15d5d76cceeb10d94f8b4543
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-12-24 14:28:33 +0530

    Number Pattern

---
+++ Patterns/Pattern32.java
@@ -0,0 +1,28 @@
+package Patterns;
+
+/*
+5
+45
+345
+2345
+12345
+ */
+public class Pattern32 {
+    public static void main(String[] args) {
+
+        int end = 1;
+        int num = 5;
+
+        for(int i=1; i<=5; i++){
+            int num2 = num;
+            for(int j=1; j<=end; j++){
+                System.out.print(num2);
+                num2++;
+            }
+            System.out.println();
+            end++;
+            num--;
+        }
+       
+    }
+}
