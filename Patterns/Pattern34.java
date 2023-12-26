bc07f1bc5ee4d9c0314965ef66d91f095eca7665
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-12-26 09:51:50 +0530

    Number Pattern

---
+++ Patterns/Pattern34.java
@@ -0,0 +1,29 @@
+package Patterns;
+
+/*
+1
+22    
+333   
+4444  
+55555 
+666666
+7777777
+88888888
+999999999
+ */
+public class Pattern34 {
+    public static void main(String[] args) {
+        
+        int end = 1;
+        int num = 1;
+
+        for(int i=1; i<=9;i++){
+            for(int j=1; j<=end; j++){
+                System.out.print(num);
+            }
+            System.out.println();
+            num++;
+            end++;
+        }
+    }
+}
