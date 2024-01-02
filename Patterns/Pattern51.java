afd8a8ffb0074460698cf7390bf78b4257e43b9a
anishasaid <116268672+anishasaid@users.noreply.github.com>
2024-01-02 10:05:29 +0530

    Character Pattern

---
+++ Patterns/Pattern51.java
@@ -0,0 +1,33 @@
+package Patterns;
+
+/*
+A
+AB   
+ABC  
+ABCD 
+ABCDE
+ABCD 
+ABC
+AB
+A
+ */
+public class Pattern51 {
+    public static void main(String[] args) {
+
+        int end = 1;
+
+        for(int i=1; i<=9; i++){
+            int num  = 1;
+            for(int j=1; j<=end; j++){
+                System.out.print((char)(num + 64));
+                num++;
+            }
+            System.out.println();
+            if(i<5){
+                end++;
+            }else{
+                end--;
+            }
+        }
+    }
+}
