3a980ea3e592fc870a7e28019d47848ba738f64d
anishasaid <116268672+anishasaid@users.noreply.github.com>
2024-01-17 13:30:57 +0530

    Character Pattern
    Pattern is : 
        N
       N
      N
     N
    N

---
+++ Patterns/Pattern61.java
@@ -0,0 +1,23 @@
+package Patterns;
+
+/*
+    N
+   N
+  N
+ N
+N
+ */
+public class Pattern61 {
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++ ){
+            for(int j=1;j<=5;j++){
+                if((j+i) == 6){
+                    System.out.print("N");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
