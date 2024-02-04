899076d6e15c18de939ee2b937931f96541d4866
anishasaid <116268672+anishasaid@users.noreply.github.com>
2024-02-04 12:51:43 +0530

    Star Pattern

---
+++ Patterns/Pattern73.java
@@ -0,0 +1,23 @@
+package Patterns;
+
+/*
+*****
+ * *
+  *
+ * *
+*****
+ */
+public class Pattern73 {
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=5; j++){
+                if(j-i == 0 || j+i == 6 || i==1 || i==5){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
