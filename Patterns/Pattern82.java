7d89a2ba5691d4e6855eeba65103bab406a7d4a0
anishasaid <116268672+anishasaid@users.noreply.github.com>
2024-02-11 17:33:14 +0530

    Number Pattern

---
+++ Patterns/Pattern82.java
@@ -0,0 +1,17 @@
+package Patterns;
+/*
+55555
+54444
+54333
+54322
+54321
+ */
+public class Pattern82 {
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++){
+            for(int j=1;j<=5; j++){
+               
+            }
+        }
+    }
+}
