63d3c3c8e90401d60766d4931c57e7d944e9aba4
anishasaid <116268672+anishasaid@users.noreply.github.com>
2024-02-28 20:08:58 +0530

    sum of array element

---
+++ Array/SumOfArryEle.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class SumOfArryEle {
+    public static void main(String[] args) {
+        
+        int arr[] = {1,2,3,4,5,6,7};
+
+        int sum  = 0;
+        for(int i=0; i<arr.length;i++){
+            sum = sum + arr[i];
+        }
+        System.out.println("Sum of Array Element is : "+sum);
+    }
+}
