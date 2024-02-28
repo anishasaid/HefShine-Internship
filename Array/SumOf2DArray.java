ab96c28d394ea5c9172283f5e9c19dd8499f2157
anishasaid <116268672+anishasaid@users.noreply.github.com>
2024-02-28 20:08:18 +0530

    Make addition of elements in 2D array.

---
+++ Array/SumOf2DArray.java
@@ -0,0 +1,35 @@
+package Array;
+// Make addtion of elements in 2D array.
+public class SumOf2DArray {
+    public static void main(String[] args) {
+        
+        int a[][] = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+
+        int b[][] = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        // create new array to store 
+        int c[][] = new int[a.length][a.length];
+
+        // perform addition and add in c Array
+        for(int i=0; i<a.length;i++){
+            for(int j=0; j<a.length;j++){
+                c[i][j] = a[i][j] + b[i][j];
+            }
+        }
+
+        //print
+        for(int i=0; i<c.length;i++){
+            for(int j=0; j<c.length;j++){
+                System.out.print(c[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
