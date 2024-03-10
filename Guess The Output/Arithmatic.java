cdfc0493e5aa9d0b9b64da7616d222c9e5724028
anishasaid <116268672+anishasaid@users.noreply.github.com>
2024-03-10 09:54:24 +0530

    Add files via upload

---
+++ Guess The Output/Arithmatic.java
@@ -0,0 +1,15 @@
+package GTO;
+
+class Arithmatic {
+    public static void main(String[] args) {
+        short s = 15;
+        int a = s;
+        s += 10;
+        System.out.println(s + ", "+ a); 
+
+        System.out.println("**************************88");
+         
+        int x = -5;
+        System.out.println(x << 1);
+    }
+}
