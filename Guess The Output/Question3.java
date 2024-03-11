603ee55e1328e86650bbca2f7c1a387dd16e7eff
anishasaid <116268672+anishasaid@users.noreply.github.com>
2024-03-11 12:25:23 +0530

    Add files via upload

---
+++ Guess The Output/Question3.java
@@ -0,0 +1,9 @@
+package GTO;
+
+public class Question3 {
+    public static void main(String[] args) {
+        int  i = 0;
+        for(System.out.println("Hi"); i<4; i++)
+        System.out.println("hello geeks");
+    }
+}
