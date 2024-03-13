7df0a7c007e35fc4d4d48ce57914c50434475262
anishasaid <116268672+anishasaid@users.noreply.github.com>
2024-03-13 23:42:24 +0530

    Add files via upload

---
+++ Guess The Output/Question7.java
@@ -0,0 +1,26 @@
+package GTO;
+
+public class Question7 {
+    public static void main(String[] args) {
+        int num1 = Integer.SIZE;
+        System.out.println("Integer: "+num1); // 32
+
+        short num2 = Short.SIZE;
+        System.out.println("Short : "+num2);  //16
+
+        byte num3 = Byte.SIZE;
+        System.out.println("Byte : " + num3); // 8
+
+        long num4 = Long.SIZE;
+        System.out.println("Long : "+ num4);  //64
+
+        float num5 = Float.SIZE;
+        System.out.println("Float : "+num5);  //32.0
+
+        double num6 = Double.SIZE;
+        System.out.println("Double : "+ num6); //64.0
+
+        char num7 = Character.SIZE;
+        System.out.println("Character : "+num7);
+    }
+}
