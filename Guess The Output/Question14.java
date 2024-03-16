db7f2cf6917ab74034d54c9b32897cef4e5fb86a
anishasaid <116268672+anishasaid@users.noreply.github.com>
2024-03-16 22:16:44 +0530

    Add files via upload

---
+++ Guess The Output/Question14.java
@@ -0,0 +1,8 @@
+package GTO;
+
+public class Question14 {
+    public static void main(String[] args) {
+        String str = "Java Concept Of The Day";
+        System.out.println(str.indexOf('a', 5));
+    }
+}
