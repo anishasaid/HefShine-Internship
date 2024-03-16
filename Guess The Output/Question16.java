8edc003f6af15f6f7d007c0beca78712ee1ce6af
anishasaid <116268672+anishasaid@users.noreply.github.com>
2024-03-16 22:17:33 +0530

    Add files via upload

---
+++ Guess The Output/Question16.java
@@ -0,0 +1,13 @@
+package GTO;
+
+public class Question16 {
+    public static void main(String[] args) {
+        int x = 20;
+        float f = 1.3f;
+        myCode(x,f);
+    }
+
+    private static void myCode(int x, float f){
+        System.out.println("A"+"B"+'A'+(x-f));
+    }
+}
