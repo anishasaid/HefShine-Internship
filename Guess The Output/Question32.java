ddca9df2eb5461ed6c98b9e51aa436185b5681b9
anishasaid <116268672+anishasaid@users.noreply.github.com>
2024-07-11 20:41:01 +0530

    Add files via upload

---
+++ Guess The Output/Question32.java
@@ -0,0 +1,10 @@
+package GTO;
+
+public class Question32 {
+    public static void main(String[] args) {
+        int i=0;
+        for(System.out.println("HI"); i<4; i++){
+            System.out.println("HELLO GEEKS");
+        }
+    }
+}
