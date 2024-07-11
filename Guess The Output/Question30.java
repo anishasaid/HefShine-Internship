d12d6cc7e95a223a9c4978374f8f02b036682e39
anishasaid <116268672+anishasaid@users.noreply.github.com>
2024-07-11 20:39:33 +0530

    Add files via upload

---
+++ Guess The Output/Question30.java
@@ -0,0 +1,11 @@
+package GTO;
+// mcq question - compilation error
+public class Question30 {
+    public static void main(String[] args) {
+        
+    }
+}
+
+enum Enums extends Question30{
+    PQR, QRS, RST, STU;
+}
